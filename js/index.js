ca8e8c3d7821d9deac755a59f769fa34fed17477
mohamed eid <mohamedwahsh070@gmail.com>
2024-11-09 20:48:35 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function playVid() {
+    let video = document.getElementById("video");
+    if(video.paused){
+        video.play();
+    }
+    else{
+        video.pause();
+    }
+}
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+  document.getElementById("fullyear").innerHTML = new Date().getFullYear();
